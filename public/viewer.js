fad42da56732ce6f8c89d46697b06038157ad01b
Adrian Zarifis <agzarifis@gmail.com>
2018-07-22 20:55:24 -0700

    Basic poll create & delete functionality

--- public/viewer.js
+++ public/viewer.js
@@ -1,34 +1,48 @@
 var token = "";
 var tuid = "";
-var ebs = "";
 
 // because who wants to type this every time?
 var twitch = window.Twitch.ext;
 
-// create the request options for our Twitch API calls
-var requests = {
-    set: createRequest('POST', 'cycle'),
-    get: createRequest('GET', 'query')
-};
+// base url for our backend
+var backendUrl = 'https://localhost:8081';
 
-function createRequest(type, method) {
+function createPollRequest(text) {
 
     return {
-        type: type,
-        url: 'https://localhost:8081/color/' + method,
-        success: updateBlock,
-        error: logError
+        type: 'POST',
+        url: backendUrl + '/poll/create',
+        data: {'text': text},
+        success: updatePoll,
+        error: logError,
+        headers: { 'Authorization': 'Bearer ' + token }
     }
 }
 
-function setAuth(token) {
-    Object.keys(requests).forEach((req) => {
-        twitch.rig.log('Setting auth headers');
-        requests[req].headers = { 'Authorization': 'Bearer ' + token }
-    });
+function queryPollRequest() {
+
+  return {
+    type: 'GET',
+    url: backendUrl + '/poll/query',
+    success: updatePoll,
+    error: logError,
+    headers: { 'Authorization': 'Bearer ' + token }
+  }
+}
+
+function clearPollRequest() {
+
+  return {
+    type: 'POST',
+    url: backendUrl + '/poll/reset',
+    success: clearPoll,
+    error: logError,
+    headers: { 'Authorization': 'Bearer ' + token }
+  }
 }
 
 twitch.onContext(function(context) {
+    // twitch.actions.requestIdShare();
     twitch.rig.log(context);
 });
 
@@ -36,41 +50,65 @@ twitch.onAuthorized(function(auth) {
     // save our credentials
     token = auth.token;
     tuid = auth.userId;
+    twitch.rig.log('upon auth, token: ' + token);
+    twitch.rig.log('upon auth, tuid: ' + tuid);
 
     // enable the button
-    $('#cycle').removeAttr('disabled');
+    $('#create').removeAttr('disabled');
+
+    // enable the text field
+    $('#input').removeAttr('disabled');
 
     setAuth(token);
-    $.ajax(requests.get);
+    $.ajax(queryPollRequest());
 });
 
-function updateBlock(hex) {
-    twitch.rig.log('Updating block color');
-    $('#color').css('background-color', hex);
+function updatePoll(poll) {
+  twitch.rig.log('Updating poll');
+  $('#poll').text(poll.text);
+
+  // enable the clear button
+  $('#clear').removeAttr('disabled');
+}
+
+function clearPoll(poll) {
+  twitch.rig.log('Clearing poll');
+  $('#poll').text(poll.text);
+
+  // disable the clear button
+  $('#clear').addAttr('disabled');
 }
 
 function logError(_, error, status) {
   twitch.rig.log('EBS request returned '+status+' ('+error+')');
 }
 
-function logSuccess(hex, status) {
-  // we could also use the output to update the block synchronously here,
-  // but we want all views to get the same broadcast response at the same time.
-  twitch.rig.log('EBS request returned '+hex+' ('+status+')');
-}
-
 $(function() {
 
-    // when we click the cycle button
-    $('#cycle').click(function() {
+    // when we click the create button
+    $('#create').click(function() {
         if(!token) { return twitch.rig.log('Not authorized'); }
-        twitch.rig.log('Requesting a color cycle');
-        $.ajax(requests.set);
+        twitch.rig.log('Creating a poll');
+        var pollText = $('#input').val();
+        $.ajax(createPollRequest(pollText));
+    });
+
+    // when we hit enter while typing in the text box
+    $("#input").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#create").click();
+        }
+    });
+
+    $("#clear").click(function() {
+      if(!token) { return twitch.rig.log('Not authorized'); }
+      twitch.rig.log('Clearing the poll');
+      $.ajax(clearPollRequest());
     });
 
     // listen for incoming broadcast message from our EBS
-    twitch.listen('broadcast', function (target, contentType, color) {
-        twitch.rig.log('Received broadcast color');
-        updateBlock(color);
+    twitch.listen('broadcast', function (target, contentType, poll) {
+        twitch.rig.log('Received broadcast poll');
+        updatePoll(poll);
     });
 });
