3facc31d40ef95a30b78ba234489438d83101f17
agzarifis <agzarifis@gmail.com>
2018-07-30 19:36:33 -0700

    DynamoDB Enabled (#7) (#8)
    * Adds listeners in viewer.js and broadcaster.js for more specific pubsub message (whisper rather than broadcaster)
    Moves jquery to attach listeners upon authorization
    Switches persistence of polls to dynamodb service
    
    * Improves logging
    
    * Fixes bug
    
    * Adds settings
    
    * Cleans up code
    Fixes cooldown logic
    
    * Adds shared.js and cleans up other js files

--- public/broadcaster.js
+++ public/broadcaster.js
@@ -1,21 +1,10 @@
-let token = "";
-let tuid = "";
+function createPollRequest(text) {
 
-// because who wants to type this every time?
-const twitch = window.Twitch.ext;
-
-// base url for our backend
-const backendUrl = 'https://localhost:8081';
-
-const noPollDefaultText = 'No poll right now';
-
-function createPollRequest(pollObj) {
 
   return {
     type: 'POST',
     url: backendUrl + '/poll/create',
     data: JSON.stringify(pollObj),
-    success: updatePoll,
     error: logError,
     contentType: "application/json; charset=utf-8",
     headers: { 'Authorization': 'Bearer ' + token }
@@ -27,7 +16,6 @@ function queryPollRequest() {
   return {
     type: 'GET',
     url: backendUrl + '/poll/query',
-    success: updatePoll,
     error: logError,
     headers: { 'Authorization': 'Bearer ' + token }
   }
@@ -38,7 +26,16 @@ function clearPollRequest() {
   return {
     type: 'POST',
     url: backendUrl + '/poll/reset',
-    success: updatePoll,
+    error: logError,
+    headers: { 'Authorization': 'Bearer ' + token }
+  }
+}
+
+function querySettingsRequest() {
+
+  return {
+    type: 'GET',
+    url: backendUrl + '/settings/query',
     error: logError,
     headers: { 'Authorization': 'Bearer ' + token }
   }
@@ -46,7 +43,7 @@ function clearPollRequest() {
 
 twitch.onContext(function(context) {
   // twitch.actions.requestIdShare();
-  twitch.rig.log(context);
+  // twitch.rig.log(context);
 });
 
 twitch.onAuthorized(function(auth) {
@@ -54,19 +51,86 @@ twitch.onAuthorized(function(auth) {
   token = auth.token;
   tuid = auth.userId;
 
-  twitch.rig.log('upon auth, tuid: ' + tuid);
+  twitch.rig.log('tuid: ' + tuid);
 
   // enable the create poll button
   $('#create').removeAttr('disabled');
 
   // enable the poll text field
   $('#input').removeAttr('disabled');
-
+  
   // enable the add option button
   $('#add_option').removeAttr('disabled');
+  
+  $(function() {
+    
+    // when we click the create button
+    $('#create').click(function() {
+      if(!token) { return twitch.rig.log('Not authorized'); }
+      twitch.rig.log('Creating a poll');
+      let pollText = $('#input').val();
+      let optionText = [];
+      $("input[id^=option]").each(function() {
+        optionText.push($(this).val());
+      });
 
-  $.ajax(queryPollRequest());
+      let pollObj = createPollObject(pollText, optionText);
+      $.ajax(createPollRequest(pollObj));
+    });
+
+    // when we hit enter while typing in the text box
+    $("#input").keyup(function(event) {
+      if (event.keyCode === 13) {
+        $("#create").click();
+      }
+    });
+    
+    //when we click the add_option button
+    $('#add_option').click(function() {
+      let lastOptionCount = parseInt($("[id^=option]").last().attr('id').slice(6)) || 0;
+      let thisOptionCount = lastOptionCount + 1;
+      $('#options').append("<li><input type='text' id='option"+thisOptionCount+"' placeholder='Option "+thisOptionCount+"'></li>");
+    });
+
+    $("#clear").click(function() {
+      if(!token) { return twitch.rig.log('Not authorized'); }
+      twitch.rig.log('Clearing the poll');
+      $.ajax(clearPollRequest());
+    });
+
+    // listen for incoming broadcast message from our EBS
+    twitch.listen('broadcast', function (target, contentType, message) {
+      twitch.rig.log('Received broadcasted '+message.type+' message');
+      parseMessage(message);
+    });
+
+    // listen for incoming whisper messages from our EBS
+    twitch.listen(tuid, function (target, contentType, message) {
+      twitch.rig.log('Received whispered '+message.type+' message for uid: '+tuid);
+      parseMessage(message);
+    });
+
+    $.ajax(querySettingsRequest());
+    $.ajax(queryPollRequest());
+  });
 });
+
+function applySetting(settingName, settingValue) {
+  if (settingName === "backgroundColor") {
+    const body = $('body');
+    let style = body.attr('style');
+    style += "background-color: "+settingValue+";";
+    body.attr('style', style);
+  }
+}
+
+function loadSettings(settingsObj) {
+  twitch.rig.log('Loading the channel\'s settings');
+  $.each(settingsObj, function(settingName, settingValue) {
+    channelSettings[settingName] = settingValue;
+    applySetting(settingName, settingValue);
+  })
+}
 
 function updatePoll(poll) {
   // erase the input field and empty options
@@ -99,57 +163,9 @@ function updatePoll(poll) {
   }
 }
 
-function logError(_, error, status) {
-  twitch.rig.log('EBS request returned '+status+' ('+error+')');
-}
-
 function createPollObject(pollText, pollOptionsText) {
   return {
     "text": pollText,
     "options": pollOptionsText
   };
 }
-
-$(function() {
-
-  //when we click the add_option button
-  $('#add_option').click(function() {
-    let lastOptionCount = parseInt($("[id^=option]").last().attr('id').slice(6)) || 0;
-    let thisOptionCount = lastOptionCount + 1;
-    $('#options').append("<li><input type='text' id='option"+thisOptionCount+"' placeholder='Option "+thisOptionCount+"'></li>");
-  });
-
-  // when we click the create button
-  $('#create').click(function() {
-    if(!token) { return twitch.rig.log('Not authorized'); }
-    twitch.rig.log('Creating a poll');
-    let pollText = $('#input').val();
-    let optionText = [];
-    $("input[id^=option]").each(function() {
-      optionText.push($(this).val());
-    });
-
-    let pollObj = createPollObject(pollText, optionText);
-    $.ajax(createPollRequest(pollObj));
-  });
-
-  // when we hit enter while typing in the text box
-  $("#input").keyup(function(event) {
-    if (event.keyCode === 13) {
-      $("#create").click();
-    }
-  });
-
-  $("#clear").click(function() {
-    if(!token) { return twitch.rig.log('Not authorized'); }
-    twitch.rig.log('Clearing the poll');
-    $.ajax(clearPollRequest());
-  });
-
-  // listen for incoming broadcast message from our EBS
-  twitch.listen('broadcast', function (target, contentType, poll) {
-    twitch.rig.log('Received broadcast poll');
-    updatePoll(poll);
-  });
-});
-
---
+++ public/shared.js
@@ -0,0 +1,31 @@
+const twitch = window.Twitch.ext;
+
+let token = "";
+let tuid = "";
+
+// base url for our backend
+const backendUrl = 'https://localhost:8081';
+
+const channelSettings = {};
+
+const noPollDefaultText = 'No poll right now';
+
+function parseMessage(message) {
+  const messageType = message.type;
+  const messageContent = message.content;
+  if (messageType === "poll") {
+    updatePoll(messageContent);
+  } else if (messageType === "settings") {
+    loadSettings(messageContent);
+  } else if (messageType === "success") {
+    updateFeedback(messageContent);
+  }
+}
+
+function updateFeedback(settingsObj) {
+  $("#feedback").text(settingsSuccessMessage);
+}
+
+function logError(_, error, status) {
+  twitch.rig.log('EBS request returned '+status+' ('+error+')');
+}
